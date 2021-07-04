4950d6dba92ccc602f82f65e6664f596ac38d3c9
xnameFV <namggg462@gmail.com>
2021-07-04 09:41:47 +0700

    remove env file from .gitignore

---
+++ env.js
@@ -0,0 +1,12 @@
+const HOST = 'http://localhost:3000'
+const FB_APP_ID = '331416541375280'
+const FB_APP_SECRET = '9e253f51a880ffac2b888f928a1b7f09'
+const APP_SECRET = 'gundam!@#cf'
+
+
+module.exports = {
+    HOST,
+    FB_APP_ID,
+    FB_APP_SECRET,
+    APP_SECRET
+}
